27e94a6b2c85f3dd681c5691d4361f319284984e
Binh Nguyen <b.nguyen.sn@gmail.com>
2016-04-06 22:41:00 +0100

    Commit

--- DailyChallenge/Hard001.py
+++ DailyChallenge/Hard001.py
@@ -5,12 +5,12 @@ from random import randint
 range_low = 1
 range_high = 100
 answer_list = ["c", "h", "l"]
+stat_efficient = True  # True = Enable statistically efficient calculations i.e. no random guesses.
 
 try_count = 0
-x = randint(range_low, range_high)
+x = int((range_high + range_low) / 2) if stat_efficient else randint(range_low, range_high)
 
 # Game starts
-print()
 input("Think of an integer between %s and %s. \nPress 'Enter' when ready." % (range_low, range_high))
 
 while True:
@@ -26,10 +26,10 @@ while True:
                 range_low = x + 1
             else:
                 range_high = x - 1
-            if range_low == range_high:
+            if range_low >= range_high:
                 print("The number must be %s. I've guessed it after %s tries! \nGame over." % (range_low, try_count))
                 break
             else:
-                x = randint(range_low, range_high)
+                x = int((range_high + range_low) / 2) if stat_efficient else randint(range_low, range_high)
     else:
         print("Please answer with either 'C', 'H' or 'L' (non-case-sensitive)")

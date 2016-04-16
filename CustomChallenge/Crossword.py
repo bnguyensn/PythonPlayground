48309dad8e500168c07b9efa0d441e1e30466c61
Binh Nguyen <b.nguyen.sn@gmail.com>
2016-04-16 01:34:50 +0100

    Commit

---
+++ CustomChallenge/Crossword.py
@@ -0,0 +1,12 @@
+import tkinter
+
+version = "1.0"
+window = tkinter.Tk()
+
+window.title("Crossword v.%s" % version)
+# window.wm_iconbitmap("...")  # Uncomment when .ico is ready
+window.geometry("640x640")
+
+
+
+window.mainloop()

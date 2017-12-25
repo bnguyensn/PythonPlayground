9113fa9c65047cdd119e57e9de06d63c11aa07c4
Binh Nguyen <b.nguyen.sn@gmail.com>
2017-12-25 23:09:07 +0000

    Updated Text Adventure's input taking module

--- ta/modules/cli.py
+++ ta/modules/cli.py
@@ -1,3 +1,5 @@
+"""Contain codes related to processing user inputs"""
+
 command_dict = {
     # List valid user inputs
 
@@ -8,4 +10,44 @@ command_dict = {
     'E': 'Looked around...'
 }
 
-# TODO
+
+def process_input(inp):
+    print(command_dict[inp])
+
+
+def input_is_valid(inp):
+    class InputInvalid(Exception):
+        pass
+
+    try:
+        if inp not in list(command_dict.keys()):
+            raise InputInvalid
+        return True
+    except InputInvalid:
+        print('Invalid command. Please try again')
+        return False
+
+
+def start_taking_input():
+    print('Welcome to Text Adventure\n\n'
+          "Escape from the dungeon. Enter 'W', 'A', 'S', 'D' to move.\n"
+          "Enter 'E' to interact.\n"
+          "Enter 'exit' to quit.\n\n")
+
+    while True:
+        try:
+            inp = input('Enter a command: ').upper()
+
+            # Check if user want to exit
+            if inp == 'EXIT':
+                raise KeyboardInterrupt
+
+            # Check if entered command is valid
+            # Process command if so, else restart the loop
+            if input_is_valid(inp):
+                process_input(inp)
+            else:
+                continue
+        except (KeyboardInterrupt, SystemError):
+            print('Goodbye!')
+            break
--- ta/program.py
+++ ta/program.py
@@ -3,10 +3,12 @@ import os.path
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import ta.modules.dungeon as dungeon
+import ta.modules.cli as cli
 
 
 def program():
     d = dungeon.create_new_dungeon(10, 10)
+    cli.start_taking_input()
 
 
 program()
